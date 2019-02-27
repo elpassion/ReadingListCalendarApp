29d1e4b88ac8c04cd7077f3500850e44e7f289dc
Dariusz Rybicki <darrarski@gmail.com>
2019-02-27 12:26:09 +0100

    Add MainViewController class with IBOutlets and IBActions

---
+++ ReadingListCalendarApp/MainViewController.swift
@@ -0,0 +1,32 @@
+import AppKit
+
+class MainViewController: NSViewController {
+    
+    @IBOutlet weak var bookmarksPathField: NSTextField!
+    @IBOutlet weak var bookmarksPathButton: NSButton!
+    @IBOutlet weak var bookmarksStatusField: NSTextField!
+    @IBOutlet weak var calendarAuthField: NSTextField!
+    @IBOutlet weak var calendarAuthButton: NSButton!
+    @IBOutlet weak var calendarSelectionField: NSTextField!
+    @IBOutlet weak var calendarSelectionButton: NSPopUpButton!
+    @IBOutlet weak var statusField: NSTextField!
+    @IBOutlet weak var synchronizeButton: NSButton!
+    @IBOutlet weak var progressIndicator: NSProgressIndicator!
+
+    @IBAction func changeBookmarksPathAction(_ sender: Any) {
+
+    }
+
+    @IBAction func authorizeCalendarAction(_ sender: Any) {
+
+    }
+
+    @IBAction func calendarSelectionAction(_ sender: Any) {
+
+    }
+
+    @IBAction func synchronizeAction(_ sender: Any) {
+
+    }
+
+}
