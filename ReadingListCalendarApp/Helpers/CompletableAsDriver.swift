52cde53b88e85a55e7ea508558009a985c280db1
Dariusz Rybicki <darrarski@gmail.com>
2019-07-20 00:47:53 +0200

    Fix threading issue in CompletableAsDriver func

--- ReadingListCalendarApp/Helpers/CompletableAsDriver.swift
+++ ReadingListCalendarApp/Helpers/CompletableAsDriver.swift
@@ -3,6 +3,7 @@ import RxSwift
 
 func asDriverOnErrorComplete(onError: ((Error) throws -> Void)? = nil) -> (Completable) -> Driver<Never> {
     return { completable in completable
+        .observeOn(MainScheduler.instance)
         .do(onError: onError)
         .asDriver(onErrorDriveWith: .empty())
     }
