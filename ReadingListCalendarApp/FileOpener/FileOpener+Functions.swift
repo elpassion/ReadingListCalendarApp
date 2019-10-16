73de37aa8f3c9a35c312997894517e725a43440c
Dariusz Rybicki <darrarski@gmail.com>
2019-10-16 22:15:23 +0200

    Migrate FileOpening extension to Combine API

--- ReadingListCalendarApp/FileOpener/FileOpener+Functions.swift
+++ ReadingListCalendarApp/FileOpener/FileOpener+Functions.swift
@@ -1,11 +1,8 @@
 import Combine
 import Foundation
 
-// TODO:
-//func openBookmarksFile(_ openerFactory: FileOpenerCreating) -> () -> Driver<URL> {
-//    return {
-//        openerFactory.createBookmarksFileOpener()
-//            .rx_openFile()
-//            .asDriver(onErrorDriveWith: .empty())
-//    }
-//}
+func openBookmarksFile(_ openerFactory: FileOpenerCreating) -> () -> AnyPublisher<URL, Never> {
+    return {
+        openerFactory.createBookmarksFileOpener().openFile()
+    }
+}
