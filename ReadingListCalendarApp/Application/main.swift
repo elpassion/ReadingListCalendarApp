8185764b3561c507ecbc3c78c66c9c366fb9d4e5
Dariusz Rybicki <darrarski@gmail.com>
2019-10-16 02:31:47 +0200

    Refactor main.swift

--- ReadingListCalendarApp/Application/main.swift
+++ ReadingListCalendarApp/Application/main.swift
@@ -3,5 +3,5 @@ import AppKit
 let app = NSApplication.shared
 let delegate = NSClassFromString("XCTestCase") == nil ? AppDelegate() : nil
 
-NSApplication.shared.delegate = delegate
+app.delegate = delegate
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
